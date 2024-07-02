17e933e9ea6e93df283d5be8c13e9b93aabd51d3
Benedict Lee <benedict_lee@imda.gov.sg>
2024-07-02 20:14:44 +0800

    Remove Webkit Testing Temporarily

--- playwright.config.ts
+++ playwright.config.ts
@@ -69,10 +69,10 @@ export default defineConfig({
       use: { ...devices['Desktop Firefox'] },
     },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
 
     /* Test against mobile viewports. */
     // {
