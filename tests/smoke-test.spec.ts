83087b7f021929a26c1d715c7ae68725de7423b1
Jackson Boey <jackson_boey@imda.gov.sg>
2024-09-17 14:28:24 +0800

    Update benchmarking tc due to change to landing page

--- tests/smoke-test.spec.ts
+++ tests/smoke-test.spec.ts
@@ -1,12 +1,12 @@
-import { test, expect } from '@playwright/test';
-import { main_page_startup } from './common';
+import {test, expect} from '@playwright/test';
+import {main_page_startup} from './common';
 import dotenv from 'dotenv';
 import path from 'path';
 
 const __dirname: string = '.'
 
 // Read from ".env" file.
-dotenv.config({ path: path.resolve(__dirname, '.env') });
+dotenv.config({path: path.resolve(__dirname, '.env')});
 
 // ---------------------------------------------------------------------------------------------------------------------
 // Test moonshot - Smoke Test
@@ -27,10 +27,10 @@ async function open_home_page_select_util_tab(page, MOONSHOT_URL: string, MOOONS
     await linkTabButton.click();
 
     // Assertion for redirecting to right page
-    await expect(page.getByRole('heading', { name: 'moonshot utilities' })).toBeVisible();
+    await expect(page.getByRole('heading', {name: 'moonshot utilities'})).toBeVisible();
 }
 
-test('Moonshot UI Smoke Test', async ({ page }) => {
+test('Moonshot UI Smoke Test', async ({page}) => {
 
     const ADDITIONAL_PARAMETERS: string | undefined = process.env.ADDITIONAL_PARAMETERS;
     const AZURE_OPENAI_URI: string | undefined = process.env.AZURE_OPENAI_URI;
@@ -52,11 +52,11 @@ test('Moonshot UI Smoke Test', async ({ page }) => {
     // Create Endpoint
     console.log('Create Endpoint')
     await page.getByRole('listitem').nth(0).click();
-    await page.getByRole('button', { name: 'Create New Endpoint' }).click();
+    await page.getByRole('button', {name: 'Create New Endpoint'}).click();
     await page.getByPlaceholder('Name of the model').click();
     await page.getByPlaceholder('Name of the model').fill(ENDPOINT_NAME);
     await page.locator('.aiv__input-container').click();
-    await page.getByRole('option', { name: 'azure-openai-connector' }).click();
+    await page.getByRole('option', {name: 'azure-openai-connector'}).click();
     await page.getByPlaceholder('URI of the remote model').click();
     await page.getByPlaceholder('URI of the remote model').fill(AZURE_OPENAI_URI!);
     await page.getByPlaceholder('Access token for the remote').click();
@@ -64,81 +64,81 @@ test('Moonshot UI Smoke Test', async ({ page }) => {
     await page.getByText('More Configs').click();
     await page.getByPlaceholder('Additional parameters').click();
     await page.getByPlaceholder('Additional parameters').fill(ADDITIONAL_PARAMETERS!);
-    await page.getByRole('button', { name: 'OK' }).click();
-    await page.getByRole('button', { name: 'Save' }).click();
+    await page.getByRole('button', {name: 'OK'}).click();
+    await page.getByRole('button', {name: 'Save'}).click();
 
     // Benchmarking
     console.log('Benchmarking')
     await page.getByRole('listitem').nth(1).click();
-    await page.getByRole('button', { name: 'Start New Run' }).click();
-    await page.getByRole('button', { name: 'Hard test sets for Common' }).click();
-    await page.getByRole('button', { name: 'MLCommons AI Safety' }).click();
-    await page.locator('.flex > .flex > svg').click();
-    await page.getByRole('main').getByRole('img').nth(2).click();
-    await page.getByText(ENDPOINT_NAME!).click();
-    await page.locator('div:nth-child(3) > .flex > svg').click();
+    await page.getByRole('button', {name: 'Start New Run'}).click();
+    await page.getByRole('button', {name: 'Trust & Safety'}).click();
+    await page.getByLabel('Select common-risk-easy').check();
+    await page.getByRole('button', {name: 'OK'}).click();
+    await page.getByLabel('Next View').click();
+    await page.getByLabel('Select '+ENDPOINT_NAME).check();
+    await page.getByLabel('Next View').click();
     await page.getByPlaceholder('Give this session a unique').click();
     await page.getByPlaceholder('Give this session a unique').fill('Test ' + Math.floor(Math.random() * 1000000000));
     await page.getByPlaceholder('Number of prompts perrecipe.').click();
     await page.getByPlaceholder('Number of prompts perrecipe.').fill('1');
-    await page.getByRole('button', { name: 'Run' }).click();
-    await expect(page.getByRole('button', { name: 'View Report' })).toBeVisible({ timeout: 60000 })
-    await page.getByRole('button', { name: 'View Report' }).click();
+    await page.getByRole('button', {name: 'Run'}).click();
+    await expect(page.getByRole('button', {name: 'View Report'})).toBeVisible({timeout: 60000})
+    await page.getByRole('button', {name: 'View Report'}).click();
     const downloadPromise = page.waitForEvent('download');
-    await page.getByRole('button', { name: 'Download Detailed Scoring JSON' }).click();
+    await page.getByRole('button', {name: 'Download Detailed Scoring JSON'}).click();
     const download = await downloadPromise;
     const download1Promise = page.waitForEvent('download');
-    await page.getByRole('button', { name: 'Download HTML Report' }).click();
+    await page.getByRole('button', {name: 'Download HTML Report'}).click();
     const download1 = await download1Promise;
-    await page.locator('main').filter({ hasText: 'Showing results forazure-' }).getByRole('link').first().click();
+    await page.locator('main').filter({hasText: 'Showing results forazure-'}).getByRole('link').first().click();
     await page.getByText(/back to home/i).click()
 
     // Red Teaming
     console.log('Red Teaming')
     await page.getByRole('listitem').nth(2).click();
-    await page.getByRole('button', { name: 'Start New Session' }).click();
+    await page.getByRole('button', {name: 'Start New Session'}).click();
     await page.getByText(ENDPOINT_NAME!).click();
     await page.locator('div:nth-child(2) > .flex > svg').click();
-    await page.getByRole('heading', { name: 'Toxic Sentence Generator' }).click();
+    await page.getByRole('heading', {name: 'Toxic Sentence Generator'}).click();
     await page.locator('div:nth-child(3) > .flex > svg').click();
     await page.getByPlaceholder('Give this session a unique').fill('Test ' + Math.floor(Math.random() * 1000000000));
-    await page.getByRole('button', { name: 'Start' }).click();
-    await page.getByRole('button', { name: 'Prompt Template' }).click();
-    await page.locator('div').filter({ hasText: /^mmlu$/ }).click();
-    await page.getByRole('button', { name: 'Use' }).click();
-    await page.getByRole('button', { name: 'Context Strategy' }).click();
-    await page.locator('div').filter({ hasText: /^Add Previous Prompt$/ }).first().click();
-    await page.getByRole('button', { name: 'Use' }).click();
+    await page.getByRole('button', {name: 'Start'}).click();
+    await page.getByRole('button', {name: 'Prompt Template'}).click();
+    await page.locator('div').filter({hasText: /^mmlu$/}).click();
+    await page.getByRole('button', {name: 'Use'}).click();
+    await page.getByRole('button', {name: 'Context Strategy'}).click();
+    await page.locator('div').filter({hasText: /^Add Previous Prompt$/}).first().click();
+    await page.getByRole('button', {name: 'Use'}).click();
     await page.getByPlaceholder('Write a prompt...').click();
     await page.getByPlaceholder('Write a prompt...').fill('Generate Something');
-    await page.getByRole('button', { name: /send/i }).click();
+    await page.getByRole('button', {name: /send/i}).click();
     // Locate the <h1> element with class "text-right" and text "You"
     const h1Element = page.locator('h1.text-right').nth(0);
 
     // Assert that the <h1> element with class "text-right" contains the text "You"
-    await expect(h1Element).toBeVisible({ timeout: 1200000 });
+    await expect(h1Element).toBeVisible({timeout: 1200000});
     // Locate the <h1> element with class "text-right" and text "You"
     const h2Element = page.locator('h1.text-left').nth(0);
 
-    await expect(h2Element).toBeVisible({ timeout: 1200000 })
+    await expect(h2Element).toBeVisible({timeout: 1200000})
     await expect(h2Element).toHaveText('AI');
 
     // Utilities
     console.log('Utilities')
     await open_home_page_select_util_tab(page, MOONSHOT_URL!, MOONSHOT_PORT_NUMBER!)
-    await page.getByRole('button', { name: 'View Prompt Templates' }).click();
-    await expect(page.locator('header').filter({ hasText: 'Prompt Templates' })).toBeVisible();
-    await expect(page.locator('li').filter({ hasText: 'tamil-templatenewsclassificationThis template is used for Tamil News' })).toBeVisible();
+    await page.getByRole('button', {name: 'View Prompt Templates'}).click();
+    await expect(page.locator('header').filter({hasText: 'Prompt Templates'})).toBeVisible();
+    await expect(page.locator('li').filter({hasText: 'tamil-templatenewsclassificationThis template is used for Tamil News'})).toBeVisible();
 
     await open_home_page_select_util_tab(page, MOONSHOT_URL!, MOONSHOT_PORT_NUMBER!)
-    await expect(page.getByRole('heading', { name: 'moonshot utilities' })).toBeVisible();
-    await page.getByRole('button', { name: 'View Context Strategies' }).click();
-    await expect(page.getByRole('heading', { name: 'Context Strategies' })).toBeVisible();
+    await expect(page.getByRole('heading', {name: 'moonshot utilities'})).toBeVisible();
+    await page.getByRole('button', {name: 'View Context Strategies'}).click();
+    await expect(page.getByRole('heading', {name: 'Context Strategies'})).toBeVisible();
     await expect(page.locator('h4').nth(0)).toContainText('Add Previous Prompt');
     await expect(page.locator('body')).toContainText('This is a sample context strategy that adds in previous prompts to the current prompt. [Default: 5]');
 })
 
-test.skip('CLI Test', async ({ page }) => {
+test.skip('CLI Test', async ({page}) => {
 
     // const moonshot_cli = spawn('python3', ['-m', 'moonshot', 'cli', 'interactive'], { cwd:"/Users/benedict/Documents/GitHub/Moonshot-Projects/moonshot" })
 
