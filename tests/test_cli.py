6d433c5aa5fc8e32148a46a4dd5be1e5fba15c4e
Jackson Boey <jackson_boey@imda.gov.sg>
2024-10-02 16:18:40 +0800

    Add benchmarking cli script

---
+++ tests/test_cli.py
@@ -0,0 +1,63 @@
+import random
+
+import pytest
+import subprocess
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # Load environment variables from .env file
+
+AZURE_OPENAI_URI = os.getenv('AZURE_OPENAI_URI')
+AZURE_OPENAI_TOKEN = os.getenv('AZURE_OPENAI_TOKEN')
+def test_cli_benchmark():
+    command = (
+        # 'cd .. &&'
+        # 'cd moonshot &&'
+        'python3 -m moonshot cli interactive'
+    )
+
+    process = subprocess.Popen(
+        command,
+        shell=True,  # Allows for complex shell commands
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        stdin=subprocess.PIPE,
+        text=True,
+        cwd="/Users/jacksonboey/PycharmProjects/moonshot",
+    )
+    # Ensure process.stdin is not None
+    if process.stdin is None:
+        raise RuntimeError("Failed to create stdin for the subprocess")
+
+    # # Example command to send to the process
+    # process.stdin.write('list_cookbooks\n')
+    # process.stdin.flush()
+
+    # Update Endpoints
+    command = 'update_endpoint azure-openai-gpt4o "[(\'name\', \'Azure OpenAI GPT4o\'), (\'uri\', \''+str(AZURE_OPENAI_URI)+'\'), (\'token\', \''+str(AZURE_OPENAI_TOKEN)+'\'), (\'params\', {\'timeout\': 300, \'allow_retries\': True, \'num_of_retries\': 3, \'temperature\': 0.5, \'model\': \'gpt-4o\'})]"\n'
+    print('Command:', command)
+    # Example command to send to the process
+    process.stdin.write(command)
+    process.stdin.flush()
+
+    # Generate a random number between 0 and 999,999,999 (inclusive)
+    random_number = int(random.random() * 1000000000)
+    nameOfRunnerFileName = "my-benchmarking-runner-" + str(random_number)
+    nameOfRunnerName = "my benchmarking runner " + str(random_number)
+
+    # Example command to send to the process
+    process.stdin.write(
+        'run_cookbook "' + nameOfRunnerName + '" "[\'chinese-safety-cookbook\']" "[\'azure-openai-gpt4o\']" -n 1 -r 1 -s "You are an intelligent AI"\n')
+    process.stdin.flush()
+
+    # Capture the output and errors
+    stdout, stderr = process.communicate()
+
+    print('Output:', stdout)
+    # Split the output into lines
+    output_lines = stdout.splitlines()
+
+    # Get the last line of the output
+    last_line = output_lines[-16]
+    print('=========================Output Last Line:', last_line)
+    assert last_line.replace(" ", "") == "CookbookResult"
