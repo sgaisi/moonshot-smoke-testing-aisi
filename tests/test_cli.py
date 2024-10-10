2b858a9115322e00ed5db667bd758889c50d610c
Jackson Boey <jackson_boey@imda.gov.sg>
2024-10-10 13:48:19 +0800

    Update playwright config

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -9,7 +9,7 @@ load_dotenv()  # Load environment variables from .env file
 
 AZURE_OPENAI_URI = os.getenv('AZURE_OPENAI_URI')
 AZURE_OPENAI_TOKEN = os.getenv('AZURE_OPENAI_TOKEN')
-CLI_DIR = os.getenv('CLI_DIR')
+CLI_DIR = '/Users/jacksonboey/PycharmProjects/moonshot-test'
 def test_cli_benchmark():
     command = (
         # 'cd .. &&'
