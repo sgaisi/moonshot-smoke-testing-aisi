b15e64ddd1a1835a8dc3f0924a2662c69f8bb5d3
Jackson Boey <jackson_boey@imda.gov.sg>
2024-10-11 09:16:17 +0800

    Update playwright for red teaming

--- tests/smoke-test.spec.ts
+++ tests/smoke-test.spec.ts
@@ -104,7 +104,9 @@ test('Moonshot UI Smoke Test', async ({page}) => {
     console.log('1')
     await page.locator('div:nth-child(2) > .flex > svg').click();
     console.log('click next already')
-    await page.locator('li').filter({ hasText: 'Toxic Sentence GeneratorThis' }).click();
+    await expect(page.getByRole('heading', { name: 'Would you like to use any of' })).toBeVisible();
+    console.log('pass page check')
+    await page.locator('li').filter({ hasText: 'Toxic Sentence GeneratorThis' }).getByRole('img').click();
     await page.locator('div:nth-child(3) > .flex > svg').click();
     console.log('2')
     await page.getByPlaceholder('Give this session a unique').fill('Test ' + Math.floor(Math.random() * 1000000000));
