5a53b2507c04a991893c89a3459d4b513b5d0487
GabrielWLM-SGAISI <gabrielwl.matienzo@ntu.edu.sg>
2025-09-30 16:29:25 +0800

    Remove prints

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -11,8 +11,10 @@ OPENAI_URI = os.getenv('OPENAI_URI')
 OPENAI_TOKEN = os.getenv('OPENAI_TOKEN')
 CLI_DIR = os.getenv('CLI_DIR')
 COMMAND = (
-        'python3 -m moonshot cli interactive'
+        'python -m moonshot cli interactive'
     )
+
+assert OPENAI_TOKEN and CLI_DIR, "Missing required environment variables"
 
 def test_cli_benchmark():
     process = subprocess.Popen(
@@ -31,8 +33,7 @@ def test_cli_benchmark():
 
     # Update Endpoints
     command = 'update_endpoint openai-gpt4o "[(\'name\', \'OpenAI GPT4o\'), (\'uri\', \''+str(OPENAI_URI)+'\'), (\'token\', \''+str(OPENAI_TOKEN)+'\'), (\'params\', {\'timeout\': 300, \'allow_retries\': True, \'num_of_retries\': 3, \'temperature\': 0.5, \'model\': \'gpt-4o\'})]"\n'
-    print('Command:', command)
-    # Example command to send to the process
+    # print('Command:', command)
     process.stdin.write(command)
     process.stdin.flush()
 
@@ -40,7 +41,6 @@ def test_cli_benchmark():
     random_number = int(random.random() * 1000000000)
     nameOfRunnerName = "my benchmarking runner " + str(random_number)
     command = 'run_cookbook "' + nameOfRunnerName + '" "[\'chinese-safety-cookbook\']" "[\'openai-gpt4o\']" -n 1 -r 1 -s "You are an intelligent AI"\n'
-    # Example command to send to the process
     process.stdin.write(command)
     process.stdin.flush()
 
@@ -71,26 +71,16 @@ def test_cli_red_teaming():
     if process.stdin is None:
         raise RuntimeError("Failed to create stdin for the subprocess")
 
-    # # Example command to send to the process
-    # process.stdin.write('list_cookbooks\n')
-    # process.stdin.flush()
-
     # Generate a random number between 0 and 999,999,999 (inclusive)
     random_number = int(random.random() * 1000000000)
     nameOfRunnerFileName = "my-red-teaming-runner-" + str(random_number)
 
     file_path = str(CLI_DIR)+"/moonshot-data-aisi/generated-outputs/runners/"+nameOfRunnerFileName+".json"
 
-    # # Example command to send to the process
-    # process.stdin.write('use_session "my-runner"\n')
-    # process.stdin.flush()
-
     command = 'new_session '+nameOfRunnerFileName+' -e "[\'openai-gpt4o\']" -c add_previous_prompt -p mmlu \n'
-    # Example command to send to the process
     process.stdin.write(command)
     process.stdin.flush()
 
-    # Example command to send to the process
     process.stdin.write('run_attack_module charswap_attack "this is my prompt"\n')
     process.stdin.flush()
 
@@ -128,7 +118,7 @@ def test_cli_agentic():
 
     # Update Endpoint
     command = 'update_endpoint openai-gpt4o "[(\'name\', \'OpenAI GPT4o\'), (\'uri\', \''+str(OPENAI_URI)+'\'), (\'token\', \''+str(OPENAI_TOKEN)+'\'), (\'params\', {\'timeout\': 300, \'allow_retries\': True, \'num_of_retries\': 3, \'temperature\': 0.5, \'model\': \'gpt-4o\'})]"\n'
-    print('Command:', command)
+    # print('Command:', command)
     process.stdin.write(command)
     process.stdin.flush()
 
